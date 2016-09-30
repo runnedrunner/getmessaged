6d71d1c452f43c4c697346525a619dd01ca91aad
runnedrunner <auszhou@nuevaschool.org>
2016-09-30 08:50:18 -0700

    Classadoo autocommit

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-addMessage()
+var quantityInput = $(".element-quantity-input")
+
+quantityInput.keydown(function(e) {
+  var keyCode = e.which;
+  var inputValue = quantityInput.val();
+
+  // enter key!
+  if (keyCode == 13) {	
+    addElementsToThePage(inputValue);
+    console.log("HI");
+  }
+})
+
+function addElementsToThePage(numberOfElements) {	  
+  $(".new-el").remove();
+  for (var i = 0; i < numberOfElements; i++) {	
+    console.log("hi");
+    $(".element-display").append("<div class='new-el'>Hi there!</div>");
+  }
+}
